c52a0e92e121460fe3d0e55c3d96a1b9bb491db3
Ivan Katjounis <ivk.msg+github@gmail.com>
2019-01-27 16:43:45 +0100

    Modification du titre de l'application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Phone catalog';
+  title = 'AngularJS Phone catalog tutorial';
 }
